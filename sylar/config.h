53612d7832a1b45620601f24c8106ec6358b8eb5
RixinLi <rli140@ucsc.edu>
2022-12-14 16:22:13 -0800

    config template for type cast

--- sylar/config.h
+++ sylar/config.h
@@ -35,7 +35,54 @@ protected:
 
 };
 
+
+// F from_type, T to_type
+template<class F, class T>
+class LexicalCast {
+public:
+    T operator() (const F& v){
+        return boost::lexical_cast<T>(v);
+    }
+
+};
+
 template<class T>
+class LexicalCast<std::string,std::vector<T>>{
+public:
+    std::vector<T> operator() (const std::string& v){
+        YAML::Node node = YAML::Load(v);
+        typename std::vector<T> vec;
+        std::stringstream ss;
+        for (size_t i=0; i<node.size();i++){
+            ss.str("");
+            ss<<node[i];
+            vec.push_back(LexicalCast<std::string,T>()(ss.str()));
+        }
+        return vec;   
+    }
+};
+
+template<class T>
+class LexicalCast<std::vector<T>,std::string>{
+public:
+    std::string operator() (const std::vector<T>& v){
+        YAML::Node node;
+        for (auto& i : v){
+            node.push_back(YAML::Load(LexicalCast<T,std::string>()(i)));
+        }
+       std::stringstream ss;
+       ss<<node;
+       return ss.str();  
+    }
+};
+
+
+
+//FromStr T operator() (const std::string&)
+//ToStr std::string operator() (const T&)
+template<class T
+        , class FromStr = LexicalCast<std::string,T>
+        , class ToStr = LexicalCast<T,std::string> >
 class ConfigVar : public ConfigVarBase{
 
 public:
@@ -50,7 +97,8 @@ public:
 
     std::string toString() override{
         try{
-            return boost::lexical_cast<std::string> (m_val);
+            // return boost::lexical_cast<std::string> (m_val);
+            return ToStr()(m_val);
         }catch(std::exception& e){
             SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) <<"ConfigVar::toString exception"<<e.what()<<" convert: "<<
             typeid(m_val).name() <<" to string"; 
@@ -60,7 +108,8 @@ public:
 
     bool fromString(const std::string& val) override {
         try{
-            m_val = boost::lexical_cast<T>(val);
+            // m_val = boost::lexical_cast<T>(val);
+            setValue(FromStr()(val));
         }catch(std::exception& e){
             SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) <<"ConfigVar::toString exception"<<e.what()<<" convert: "<<
             typeid(m_val).name(); 
--- tests/test_config.cc
+++ tests/test_config.cc
@@ -6,6 +6,8 @@ sylar::ConfigVar<int>::ptr g_int_value_config = sylar::Config::Lookup("system.po
 
 sylar::ConfigVar<float>::ptr g_float_value_config = sylar::Config::Lookup("system.value",(float)10.2f,"system value");
 
+sylar::ConfigVar<std::vector<int>>::ptr g_int_vec_value_config = sylar::Config::Lookup("system.int_vec",std::vector<int>{1,2},"system int vec");
+
 void print_yaml(const YAML::Node& node, int level) {
     if(node.IsScalar()){
          SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << std::string(level*4,' ') << node.Scalar() << " - "<< node.Type()<< " - "<<level;
@@ -39,11 +41,21 @@ void test_config(){
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "before: "<< g_int_value_config->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "before: "<<g_float_value_config->toString();
 
+    auto v = g_int_vec_value_config->getValue();
+    for (auto& i :v){
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT())<<"before int_vec: "<<i;
+    }
+
     YAML::Node root = YAML::LoadFile("/home/lrx/ServerFrameWork/bin/conf/log.yml");
     sylar::Config::LoadFromYaml(root);
 
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "after: "<< g_int_value_config->getValue();
     SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "after: "<<g_float_value_config->toString();
+
+    v = g_int_vec_value_config->getValue();
+    for (auto& i :v){
+        SYLAR_LOG_INFO(SYLAR_LOG_ROOT())<<"after int_vec: "<<i;
+    }
 
 }
 
