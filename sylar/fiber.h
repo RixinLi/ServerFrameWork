1b212592bc41ebd6c463e1cae6bde9958d1918b2
RixinLi <rli140@ucsc.edu>
2022-12-22 16:03:40 -0800

    fiber 协程引入

---
+++ sylar/fiber.h
@@ -0,0 +1,64 @@
+#ifndef __SYLAR_FIBER_H__
+#define __SYLAR_FIBER_H__
+
+#include <memory>
+#include "thread.h"
+#include <ucontext.h>
+#include <functional>
+
+namespace sylar{
+
+class Fiber: public std::enable_shared_from_this<Fiber>{
+public:
+    std::shared_ptr<Fiber> ptr;
+
+    enum State{
+        INIT,
+        HOLD,
+        EXEC,
+        TERM,
+        READY
+    };
+};
+
+private:
+    Fiber();
+
+public:
+    Fiber(std::function<void()>cb, size_t stacksize = 0);
+    ~Fiber();
+
+    // 重置协程函数，并重置状态
+    // INIT，TERM
+    void reset(std::function<void()> cb);
+    // 切换到当前协程执行
+    void swapIn();
+    // 切换到后台
+    void swapOut();
+
+public:
+    // 返回当前协程
+    static Fiber::ptr GetThis();
+    // 设置当前协程
+    void SetThis(Fiber* f);
+    // 协程切换到后台，并且设置为ready状态
+    static void YieldToReady();
+    // 协程切换到后台，并且设置为hold状态
+    static void YieldToHold();
+    // 总协程数
+    static uint64_t TotalFibers();
+
+    static void MainFunc();
+
+private:
+    uint64_t m_id = 0;
+    uint32_t m_stakesize = 0;
+    State m_state = INIT;
+
+    ucontext_t m_ctx;
+    void* m_stack = nullptr;
+
+    std::function<void()> m_cb;
+}
+
+#endif
